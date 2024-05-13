2373b3d5de6c92f42611b78b92184abbdd81529a
Eddienator <eddiemskinnar@gmail.com>
2024-05-13 14:47:09 +0200

    yes

--- Code/settings.py
+++ Code/settings.py
@@ -5,6 +5,6 @@ from random import randint, choice
 from pygame import Vector2 as Vector
 from os.path import join
 
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 1200
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
 FRAMERATE = 144
